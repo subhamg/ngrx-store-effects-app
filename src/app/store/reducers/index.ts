30b7d8da2b4f2b05664e316a8cbc4ee8adec3168
Subham Goyal <subhamiitk@gmail.com>
2019-09-09 12:34:13 +0530

    custom router serializer

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -1,7 +1,12 @@
-import { Params } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Params
+} from '@angular/router';
 import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
 
 import * as fromRouter from '@ngrx/router-store';
+import { RouterStateSerializer } from '@ngrx/router-store';
 
 export interface RouterStateUrl {
   url: string;
@@ -20,3 +25,19 @@ export const reducers: ActionReducerMap<State> = {
 export const getRouterState = createFeatureSelector<
   fromRouter.RouterReducerState
 >('routerReducer');
+
+export class CustomSerializer
+  implements fromRouter.RouterStateSerializer<RouterStateUrl> {
+  serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+    const { url } = routerState;
+    const { queryParams } = routerState.root;
+
+    let state: ActivatedRouteSnapshot = routerState.root;
+    while (state.firstChild) {
+      state = state.firstChild;
+    }
+    const { params } = state;
+
+    return { url, queryParams, params };
+  }
+}
