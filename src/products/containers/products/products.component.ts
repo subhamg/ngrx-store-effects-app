59bfab00f365f83e5a07ed03a6fdee15ffabd242
Subham Goyal <subhamiitk@gmail.com>
2019-09-10 19:26:23 +0530

    pizzas guard

--- src/products/containers/products/products.component.ts
+++ src/products/containers/products/products.component.ts
@@ -33,7 +33,6 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit() {
     this.pizzas$ = this.store.select<any>(fromStore.getAllPizzas);
-    this.store.dispatch(new fromStore.LoadPizzas());
     this.store.dispatch(new fromStore.LoadToppings());
   }
 }
