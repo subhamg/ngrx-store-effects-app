1d484a305b67ca192af7b2a2faf73b5ffd9f4c14
Subham Goyal <subhamiitk@gmail.com>
2019-09-10 09:33:24 +0530

    Toppings effect

---
+++ src/products/store/effects/toppings.effect.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+import { Effect, Actions } from '@ngrx/effects';
+import { map, switchMap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs/Observable/of';
+
+import * as toppingActions from '../actions/toppings.action';
+import * as fromServices from '../../services';
+
+@Injectable()
+export class ToppingsEffects {
+  constructor(
+    private action$: Actions,
+    private toppingService: fromServices.ToppingsService
+  ) {}
+
+  @Effect()
+  loadTopping$ = this.action$.ofType(toppingActions.LOAD_TOPPINGS).pipe(
+    switchMap(() => {
+      return this.toppingService.getToppings().pipe(
+        map((toppings) => new toppingActions.LoadToppingsSuccess(toppings)),
+        catchError((error) => of(new toppingActions.LoadToppingsFail(error)))
+      );
+    })
+  );
+}
