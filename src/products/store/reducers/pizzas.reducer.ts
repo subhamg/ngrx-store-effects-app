a1e62e78770f59ae8c46b1724ed8b443fd604700
Subham Goyal <subhamiitk@gmail.com>
2019-09-10 13:53:15 +0530

    remove pizza

--- src/products/store/reducers/pizzas.reducer.ts
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -69,6 +69,15 @@ export function reducer(
         entities
       };
     }
+
+    case fromPizzas.REMOVE_PIZZA_SUCCESS: {
+      const pizza = action.payload;
+      const { [pizza.id]: removed, ...entities } = state.entities;
+      return {
+        ...state,
+        entities
+      };
+    }
   }
 
   return state;
